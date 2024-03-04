97dc16665f031ac6c8bf0d07966c2d7117f866d6
lgkimisis <146369762+lgkimisis@users.noreply.github.com>
2024-03-04 11:12:50 +0100

    Update FE_solver_v3b.m

--- VIV_sFOM_POD_inference_codes/FE_solver_v3b.m
+++ VIV_sFOM_POD_inference_codes/FE_solver_v3b.m
@@ -15,8 +15,8 @@ ind_bp=unique([ind_b1; ind_b2; ind_b3; ind_b4]); %boundary points (complete doma
 i_del=ismember(etri(:,1),ind_bp);
 ind_b=find(i_del==1); %Indices of boundary edges in etri
 
-%The remain indices correspond to ellipse points
-ind_e1=find(i_del==0); %Indices of ellipse edges in etri
+%The remainING indices correspond to solid boundary
+ind_e1=find(i_del==0); 
 
 edge_e=etri(ind_e1,:);
 edge_b=etri(ind_b,:);
@@ -24,53 +24,32 @@ edge_b=etri(ind_b,:);
 %boundary values for displacement
 dbx1=ones(size(ind_e1,1),1);
 dbx2=zeros(size(ind_b,1),1);
-
-%First ellipse BCs then domain
 g1=[dbx1.' dbx2.'];
-%Correct indexing of BCs to points
-
 ind_BC1=unique(edge_e);
 ind_BC2=unique(edge_b);
 
-ind_BC=[ind_BC1; ind_BC2]; %First ellipse then domain
+ind_BC=[ind_BC1; ind_BC2];
 
 %We want to find the points "close" to the body
-%There we will have both linear and quadratic terms
-
-%<4
 ind_cl1=find((points(:,1)-xc).^2/Rx^2+(points(:,2)-yc).^2/Ry^2<2);
-
 k_cl = boundary(points(ind_cl1,1),points(ind_cl1,2));
 k_cl=ind_cl1(k_cl);
-% figure(2)
-% plot(points(k,1),points(k,2),'og','LineWidth',4)
-% hold on
-% ind_cl2=find(points(:,1)>xc & abs(points(:,2)-yc)<3*Ry);
-
 ind_cl=ind_cl1; %unique([ind_cl1; ind_cl2]);
 
-%--------------------------%
-%Since the problem is linear we just need to solve it once!
+%Laplace solver for grid deformation
 nodes=tria.';
 p=points.';
 e=[edge_e.' edge_b.'];
-
-
 [ijunk,nelem] = size(nodes); %elements
 [ijunk,nnode] = size(p); %nodes
-
 gk=zeros(nnode,nnode); %matrix A
 gf = zeros(nnode,1); %vector b
-
-
 for nel = 1:nelem % Begin to assemble by element.
-    
     for j=1:3 % The coordinates of the nodes in the
         jj = nodes(j,nel); % element.
         xx(j) = p(1,jj); %x position
         yy(j) = p(2,jj); %y position
     end
-    
     for i=1:3
         j = i+1 - fix((i+1)/3)*3; %nearest int->0
         if j == 0
@@ -80,14 +59,13 @@ for nel = 1:nelem % Begin to assemble by element.
         if m == 0
             m = 3;
         end
-        %we build each test function
+        %compute each test function
         a(i) = xx(j)*yy(m) - xx(m)*yy(j);
         b(i) = yy(j) - yy(m);
         c(i) = xx(m) - xx(j);
     end
     
     delta = ( c(3)*b(2) - c(2)*b(3) )/2.0; % Area of element
-    
     for ir = 1:3
         ii = nodes(ir,nel);
         for ic=1:3
@@ -115,10 +93,4 @@ for i=1:npres
 end
 
 u=gk\gf; % Solve the linear system.
-
-figure(1)
-triplot(tria,points(:,1)+u*0.1,points(:,2)+u*0.1,'b');
-hold on
-triplot(tria,points(:,1),points(:,2),'k');
-hold off
 end
